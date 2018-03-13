a2eddbd0105a4d289a6005e9bb6f5aef0bc8e4d4
Marijn Kuijpers <I366050@fhict.nl>
2018-03-13 13:56:40 +0100

    Add Integer and floating point values

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,8 +3,6 @@ package main;
 import consoleTable.ConsoleTable;
 import dataTable.DataTable;
 
-import java.io.Console;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -12,8 +10,15 @@ public class Main {
         System.out.println();
         System.out.println("Integer Types:");
         DataTable table = createIntDataTable();
-	    ConsoleTable.drawTable(table, 80);
-
+	    ConsoleTable.drawTable(table, 84);
+	
+	    System.out.println();
+	    System.out.println("Floating Point Types:");
+	    table = createFloatDataTable();
+	    ConsoleTable.drawTable(table, 96);
+	    System.out.printf("A float uses %s bits", Float.SIZE);
+	    System.out.println();
+	    System.out.printf("A double uses %s bits", Double.SIZE);
     }
     
     private static DataTable createIntDataTable(){
@@ -23,9 +28,21 @@ public class Main {
     	table.addColumn("Min Value");
     	table.addColumn("Max Value");
     	table.addRow("Byte", "byte", Byte.MIN_VALUE, Byte.MAX_VALUE);
+	    table.addRow("Character", "char", (int)Character.MIN_VALUE, (int)Character.MAX_VALUE);
     	table.addRow("Short", "short", Short.MIN_VALUE, Short.MAX_VALUE);
     	table.addRow("Integer", "int", Integer.MIN_VALUE, Integer.MAX_VALUE);
+    	table.addRow("Long", "long", Long.MIN_VALUE, Long.MAX_VALUE);
     	return table;
-    	
+    }
+    
+    private static DataTable createFloatDataTable(){
+    	DataTable table = new DataTable();
+    	table.addColumn("Data Type");
+    	table.addColumn("Min Value");
+    	table.addColumn("Max Value");
+    	table.addColumn("Epsilon");
+    	table.addRow("Float", Float.MIN_VALUE, Float.MAX_VALUE, Float.MIN_NORMAL);
+    	table.addRow("Double", Double.MIN_VALUE, Double.MAX_VALUE, Double.MIN_NORMAL);
+    	return table;
     }
 }
